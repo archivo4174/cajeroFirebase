9f25890564038c0dfd071069e2d2ce68ac14089e
archivo4174 <45568946+archivo4174@users.noreply.github.com>
2019-01-23 16:56:14 -0600

    se actualizo

--- scriptfire.js
+++ scriptfire.js
@@ -1,7 +1,4 @@
 
-(function(){
-
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDgRaQ97v5F1pKwvg36Blg2HllKVEQUOVw",
@@ -12,11 +9,41 @@
     messagingSenderId: "105811925026"
   };
   firebase.initializeApp(config);
-//get elements
-  const preObject = document.getElementById('object');
-  // create references
-  const dbRefObject = firebase.database().ref().child('object');
+    function getID(id) {
+      return document.getElementById(id).value;
+      
+    }
+    function agregardatos(id,result){
+      return document.getElementById(id).value=result;
+    }
+    function arrayJSON(nombre,descripcion,precio,imagenurl){
+      var data ={
+        nombre:nombre,
+        descripcion,descripcion,
+        precio:precio,
+        imagenurl:imagenurl
+      };
+      return data;
+    };
+    function insertTask(){
+      var id = getID("id");
+      var  nombre = getID("nombre");
+      var descripcion = getID("descripcion");
+       var precio = getID("precio");
+       var imagenurl = getID("imagenurl");
+        if (id.length==0 || nombre.length==0 || precio.length==0  || descripcion.nombre==0){
+        alert("llena los campos");
+       }else{
+        var arrayData = arrayJSON(nombre,descripcion,precio,imagenurl);
+        console.log(arrayData)
+        var task = firebase.database().ref("taks/"+id);
+        task.set(arrayData);
+        alert("se guardo el producto");
+        agregardatos("id","");
+        agregardatos("nombre","");
+        agregardatos("descripcion","");
+        agregardatos("precio","");
+       }
+    }
 
 
-  dbRefObject.on('value', snap => )
-}());

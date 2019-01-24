63420815a52d265d583f58118e68e5d2b875e708
archivo4174 <45568946+archivo4174@users.noreply.github.com>
2019-01-23 23:28:36 -0600

    se actualizo

--- scriptfire.js
+++ scriptfire.js
@@ -16,6 +16,10 @@
     function agregardatos(id,result){
       return document.getElementById(id).value=result;
     }
+
+    function innerHTML(id,result){
+      return document.getElementById(id).innerHTML+=result;
+    }
     function arrayJSON(nombre,descripcion,precio,imagenurl){
       var data ={
         nombre:nombre,
@@ -45,5 +49,38 @@
         agregardatos("precio","");
        }
     }
+    function table(nombre,precio,descripcion){
+      return '<div class="tar1 mt-4 col-lg-3 col-md-6">'+
+       '<div class="card animated bounceInUp  style="width: 18rem;" >'+
+          '<img class="card-img-top" src=" http://drive.google.com/uc?export=view&id=1QlPubZyUlnIhrDEgeT_RNSNfVlg-z7_s">'+
+          '<div class="card-body">'+
+            '<h5 class="card-title">'+nombre+'</h5>'+
+            '<span >Precio:</span>'+
+            '<h6 style="width:75%; float:right;"  class="card-title">'+precio+'</h6>'+
+            
+            '<p style="width:100%; float:right;" class="card-text">'+descripcion+'</p>'+
+            '<div>'+
+              '<div>'+
+            '<input id="" class="form-control colorful" type="number" value="1" min="1"/>'+
+            '<a " class="btn btn-ttc" id="agregar">'+
+            '<img src="imagenes/car.png">'+
+            '</a>'+
+              '</div>'+
+          '</div>'+
+        '</div>'+
+      '</div>'+      
+    '</div>';
+    }
+    function whatchTabs(){
+      var task = firebase.database().ref("task/");
+      task.on("clild_added",function(data){
+        var taskValue = data.val();
+        var results = table(taskValue.nombre,taskValue.descripcion,taskValue.precio);
+        innerHTML("listado",results);
+        console.log(results);
+      });
+        
 
+      
+    }
 
